bf15097c989b193be8bcbcdbbb7674f732bae661
Justin Pallas <justin.pallas@t-online.de>
2022-10-27 17:46:36 +0200

    restructure and add basic masking threshold calculation for narrow band noise

---
+++ app.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as mticker
+import calculation as calc
+import visualisation as graph
+
+freq_center = 1000 # Hz
+volume = 80 # dB
+
+graph.render_plots(freq_center, volume)
+graph.draw_plots()
--- calculation.py
+++ calculation.py
@@ -10,6 +10,29 @@ def conv_to_freq(bark):
     freq = (1960*(bark+0.53))/(26.28-bark)
     return freq
 
+
+def masked_threshold_low(frequency, volume, freq_center):
+    slope = 27 # Steigung S1
+    center = conv_to_bark(freq_center) # Bandmittenfrequenz in Bark
+    bark = conv_to_bark(frequency) # jeweilige Frequenz in Bark
+    # Berechnung des Schnittpunkts mit der X-Achse
+    n = -slope * center + volume    
+    zero = (-n)/slope
+    # Berechnung und Ausgabe des entsprechenden Pegels der Mithörschwelle
+    level = slope * (bark - zero)
+    return level
+
+def masked_threshold_high(frequency, volume, freq_center):
+    slope = -(24+(0.23/(freq_center/1000))-0.2*volume) # Steigung S2
+    center = conv_to_bark(freq_center) # Bandmittenfrequenz in Bark
+    bark = conv_to_bark(frequency) # jeweilige Frequenz in Bark
+    # Berechnung des Schnittpunkts mit der X-Achse
+    n = -slope * center + volume    
+    zero = (-n)/slope
+    # Berechnung und Ausgabe des entsprechenden Pegels der Mithörschwelle
+    level = slope * (bark - zero)
+    return level
+
 # Berechnung der Ruhehörschwelle von gegebenen Frequenzen
 def threshold_in_quiet(frequency):
     # Formel aus Skript TT2 Seite 23
