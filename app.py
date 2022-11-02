e2f491edfa551ded9dc6a7d4c405aa11d5a3ea9f
Justin Pallas <justin.pallas@t-online.de>
2022-11-02 17:42:40 +0100

    add plot with bark scale

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import calculation as calc
 import visualisation as graph
 import data as data
 
-freq_center = 4000 # Hz
-volume = 100 # dB
+freq_center = 1000 # Hz
+volume = 60 # dB
 
 #x = np.geomspace(1, 20000, 100)
 x = data.samples()
--- visualisation.py
+++ visualisation.py
@@ -4,38 +4,51 @@ import matplotlib.ticker as mticker
 import calculation as calc
 import data as data
 
-fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(9,5))
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(9,9))
 
 # ------ Graphische Darstellung -------- 
 
-#ax1 = axs[0]
-#ax2 = axs[1]
-ax2 = axs
+ax1 = axs[0]
+ax2 = axs[1]
 
 def render_plots(x, freq_center, volume):
     # -- Diagramm für physikalisches Eingangssignal --
     # Allgemein
-    # ax1.grid(True)
-    # ax1.set_title("Physikalisches Signal")
+    ax1.grid(True)
+    ax1.set_title("Darstellung in der Tonheit")
     
-    # # X-Achse
-    # ax1.set_xlabel("Frequenz f [Hz]")
-    # ax1.set_xlim([16, 22000])
-    # ax1.set_xscale('symlog')
-    # ax1.set_xticks([16, 31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000])
-    # ax1.get_xaxis().set_major_formatter(mticker.ScalarFormatter())
+    # X-Achse
+    ax1.set_xlabel("Tonheit Z [Bark]")
+    ax1.set_xlim([0, 24])
+    #ax1.set_xscale('symlog')
+    ax1.set_xticks([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24])
+    #ax1.get_xaxis().set_major_formatter(mticker.ScalarFormatter())
     
-    # # Y-Achse
-    # ax1.set_ylabel("Pegel L [dB]")
-    # ax1.set_ylim([-10, 130])
+    # Y-Achse
+    ax1.set_ylabel("Pegel L [dB]")
+    ax1.set_ylim([-10, 130])
 
-    # # Darstellung des bestimmten Terzbandes
-    # line1, = ax1.plot(calc.signal(freq_center, volume, 'x'), calc.signal(freq_center, volume, 'y'))
+    # Darstellung der Ruhehörschwelle
+    line1, = ax1.plot(calc.conv_to_bark(data.tiq_freq), data.tiq_level, 'k--')
+
+    # Darstellung des bestimmten Terzbandes
+    line2, = ax1.plot((calc.conv_to_bark(calc.signal(freq_center, volume, 'x'))), calc.signal(freq_center, volume, 'y'))
+
+    # Versuch Darstellung MHS von SBR
+    thresh = calc.masked_threshold(x, volume, freq_center)
+    line3, = ax1.plot(calc.conv_to_bark(x), thresh, 'r')
+
+    # Beschriftung der Graphen
+    line1.set_label('Ruhehörschwelle nach DIN EN ISO 389-7:2020-06')
+    line2.set_label('Terzrauschen mit fm = ' + str(freq_center) + ' Hz und L = ' + str(volume) + ' dB')
+    line3.set_label('Mithörschwelle')
+    ax1.legend()
+
 
     # -- Diagramm mit gehörgerechter Darstellung des Signals --
     # Allgemein
     ax2.grid(True)
-    ax2.set_title("Gehörgerechte Darstellung")
+    ax2.set_title("Darstellung in der Frequenz")
     
     # X-Achse
     ax2.set_xlabel("Frequenz f [Hz]")
